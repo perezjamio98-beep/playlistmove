0d226fa184fdd2b0b53cc90a0f74c435f49c4c40
jonastorreskarina-jpg <jonastorreskarina@gmail.com>
2026-07-18 00:20:09 -0500

    Mostrar owner y usuario

--- server.js
+++ server.js
@@ -308,9 +308,21 @@ console.log("PASO 1");
 const sourcePlaylist =
   await spotifyApi.getPlaylist(playlistId);
 
+  
+
 console.log("PASO 1 OK");
 
 console.log("OWNER:", sourcePlaylist.body.owner.id);
+
+res.send(`
+<pre>
+OWNER: ${sourcePlaylist.body.owner.id}
+USUARIO: ${(await spotifyApi.getMe()).body.id}
+PLAYLIST: ${playlistId}
+</pre>
+`);
+
+return;
 
 const me = await spotifyApi.getMe();
 
